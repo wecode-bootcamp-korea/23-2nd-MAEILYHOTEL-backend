0dc48bd4b440a85e97e566c1942f29ba3efd1521
최민 <cm9542@gmail.com>
2021-08-19 10:34:56 +0900

    [23기 최민] Add: 카카오 소셜로그인 기능, 로그인 데코레이터 추가,     unittest

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+import json, jwt
 
-# Create your tests here.
+from django.http.response import JsonResponse
+
+from users.models     import User, UserLevel
+
+from django.test import TestCase, Client
+from unittest.mock import patch, MagicMock
+from my_settings import SECRET_KEY
+
+class KaKaoSignInTest(TestCase):
+    def setUp(self):
+        UserLevel.objects.create(name='silver', discount=0)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        UserLevel.objects.all().delete()
+    
+    @patch("users.views.requests")
+    def test_kakao_signin_new_user_success(self, mocked_requests):
+        class KaKaoResponse:
+            def json(self):
+                return {
+					"id"           : 1234567,
+					"kakao_account": { 
+					        "profile" : {"nickname": "가나다"},
+					        "email"   : "sample@sample.com"
+                            }
+                }
+            status_code = 200
+
+        client = Client()
+        mocked_requests.get = MagicMock(return_value = KaKaoResponse())
+        headers = {"Authorization": "가짜 토큰"}
+        response = client.post('/users/kakao', **headers, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+
+        token = response.json()['token']
+        user  = jwt.decode(token, SECRET_KEY, algorithms='HS256')['id']
+        kakao_id = User.objects.get(id=user).kakao_id
+
+        self.assertEqual(kakao_id, '1234567')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+import json, jwt, requests
 
-# Create your views here.
+
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User, UserLevel
+from my_settings  import SECRET_KEY
+
+class KaKaoSignInView(View):
+    def post(self, request):
+        try:
+            access_token = request.headers.get('Authorization')         
+            profile_response = requests.get(    
+                "https://kapi.kakao.com/v2/user/me",
+                headers = {"Authorization" : f"Bearer {access_token}"},
+                timeout = 3
+            )
+            
+            if not profile_response.status_code == 200:
+                return JsonResponse({"message" : "INVALID_TOKEN"}, status = 400)
+
+            profile_json  = profile_response.json()
+            kakao_id      = profile_json.get("id")
+            kakao_account = profile_json.get("kakao_account")
+
+            kakao_profile = kakao_account.get("profile")
+            email         = kakao_account.get("email")
+            nickname      = kakao_profile.get("nickname")           
+
+            user, created = User.objects.get_or_create(
+                    kakao_id  = kakao_id,
+                    defaults = {
+                        'email'    : 'email',
+                        'nickname' : 'nickname',
+                        'point'    : 1000000,
+                        'userlevel': UserLevel.objects.get(name='silver')
+                    }
+                )
+            user.email    = email
+            user.nickname = nickname
+            user.save()
+            token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm = 'HS256')
+            return JsonResponse({
+                "name"     : user.nickname,
+                "userlevel": user.userlevel.name,
+                "point"    : user.point,
+                "agreement": user.agreement,
+                "token"    : token}, status = 200)
+        except KeyError:
+            return JsonResponse({"message" : "INVALID_TOKEN"}, status = 400)
+
+
