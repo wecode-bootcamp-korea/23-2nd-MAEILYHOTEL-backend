5a51c92b441eb4abfe8604018b70176c20e21a5f
최민 <cm9542@gmail.com>
2021-08-22 18:47:53 +0900

    [23기 최민]Add : Staytype 상세페이지 기능 추가

--- stays/tests.py
+++ stays/tests.py
@@ -1,3 +1,63 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+from datetime    import time
+
+from stays.models import *
+
+class StaysViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name="호텔")
+        staytype = Staytype.objects.create(
+            name        = "Ahotel",
+            address     = "testaddress",
+            longitude   = 111.111,
+            latitude    = 111.111,
+            description = "설명입니다",
+            category    = category
+            )
+        facility = Facility.objects.create(name="Facility")
+        StaytypeFacility.objects.create(facility=facility,staytype=staytype)        
+        StaytypeImage.objects.create(image_url="url",staytype=staytype)
+        room = Room.objects.create(name="Aroom",quantity=10,image_url="url",people=2,staytype=staytype)
+        RoomOption.objects.create(name="숙박", price=10000, check_in=time(15,00) ,check_out=time(11,00), room=room)
+        
+    def tearDown(self):
+        RoomOption.objects.all().delete()
+        Room.objects.all().delete()
+        StaytypeImage.objects.all().delete()
+        StaytypeFacility.objects.all().delete()
+        Facility.objects.all().delete()
+        Staytype.objects.all().delete()
+        Category.objects.all().delete()
+
+    def test_staydetailview_get_success(self):
+        client   = Client()
+        response = client.get('/stays/1')
+        self.assertEqual(response.json(),
+        {
+            "category": "호텔",
+            "name": "Ahotel",
+            "images": [
+                "url"
+            ],
+            "total_rooms": 10,
+            "facilities": [
+                "Facility"
+            ],
+            "address": "testaddress",
+            "long": "111.1110000",
+            "lat": "111.1110000",
+            "description": "설명입니다",
+            "check_in": "15:00:00",
+            "check_out": "11:00:00"
+        }
+                )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_staydetailview_invalid_id_failed(self):
+        client   = Client()
+        response = client.get('/stays/50')
+        self.assertEqual(response.status_code, 404)
+
--- stays/views.py
+++ stays/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Sum
 
-# Create your views here.
+from stays.models    import Staytype, RoomOption
+
+class StayView(View):
+    def get(self, request, stay_id):
+        if not Staytype.objects.filter(id = stay_id).exists():
+            return JsonResponse({"message":"INVALID_ID"}, status=404)
+            
+        stay = Staytype.objects.get(id = stay_id)
+        room = RoomOption.objects.filter(name='숙박', room__staytype=stay)
+        data = {
+            "category"   : stay.category.name,
+            "name"       : stay.name,
+            "images"     : [stayimage.image_url for stayimage in stay.staytypeimage_set.all()],
+            "total_rooms": stay.room_set.all().aggregate(total_rooms=Sum('quantity'))['total_rooms'],
+            "facilities" : [facility.name for facility in stay.facility.all()],
+            "address"    : stay.address,
+            "long"       : stay.longitude,
+            "lat"        : stay.latitude,
+            "description": stay.description,
+            "check_in"   : room[0].check_in,
+            "check_out"  : room[0].check_out
+        }
+        return JsonResponse(data, status=200)
